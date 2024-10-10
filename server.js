8a1bc11f3511505b8d7a19e857b13b501791db24
Vinoth-Kumar-N <vinothkumarn22it@srishakthi.ac.in>
2024-10-10 19:02:52 +0530

    server setted up cors configured

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const cors = require('cors');
 
 app.use(cors({
-    origin: 'http://localhost:5173', 
+    origin: 'https://mission-possible-frontend.vercel.app', 
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }));
